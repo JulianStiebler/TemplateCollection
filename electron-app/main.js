b20f1c8991cf419e15b16f1a28dbb0acde883b97
stblr <68881884+JulianStiebler@users.noreply.github.com>
2024-06-11 08:02:59 +0200

    Electron template

---
+++ electron-app/main.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false, // Disable the title bar
+    backgroundColor: '#333333', // Set background color
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+
+  // IPC handlers for window controls
+  ipcMain.on('minimize-window', () => {
+    win.minimize();
+  });
+
+  ipcMain.on('maximize-window', () => {
+    if (win.isMaximized()) {
+      win.unmaximize();
+    } else {
+      win.maximize();
+    }
+  });
+
+  ipcMain.on('close-window', () => {
+    win.close();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
